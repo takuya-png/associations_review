421f5dbcc0aee498df1a3d018f3d537095ee04e0
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-19 07:39:33 +0900

    一対多のアソシエーション実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+  private
+  def login_required
+    redirect_to new_session_path unless current_user
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save 
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
+
+  def edit
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
